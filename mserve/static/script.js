ed4705f96f38b534c9c572787cbc6427ab4178a3
Nick Spinale <spinalen@carleton.edu>
2017-01-10 15:13:30 -0500

    add lookup for releases

--- mserve/static/script.js
+++ mserve/static/script.js
@@ -1,27 +1,40 @@
-function format(release) {
-    return [ '<tr>'
-           ,    '<td>' + release.artist + '</td>'
-           ,    '<td><a href="/download/' + release.id + '">' + release.title + '</a></td>'
-           ,    '<td>' + release.year + '</td>'
-           ,    '<td>' + release.genre + '</td>'
-           , '</tr>'
-           ].join('');
-}
+$(document).ready(function() {
 
-function get_params() {
-    return {
-        artist: $('#artist').val(),
-        title: $('#title').val(),
-        genre: $('#genre').val(),
-        year_from: $('#year_from').val(),
-        year_to: $('#year_to').val(),
-    };
-}
+    console.log('hi');
+    function format(release) {
+        return [ '<tr>'
+               ,    '<td>' + release.artist + '</td>'
+               ,    '<td>'
+               ,        '<a href="/download/' + release.id + '">'
+               ,            '<span class="glyphicon glyphicon-download-alt"></span>'
+               ,        '</a>'
+               ,        '&nbsp'
+               ,        '<a href="https://musicbrainz.org/release/' + release.id + '" target="_blank">'
+               ,            release.title
+               ,        '</a>'
+               ,    '</td>'
+               ,    '<td>' + release.year + '</td>'
+               ,    '<td>' + release.genre + '</td>'
+               , '</tr>'
+               ].join('');
+    }
 
-$('#search-form').submit(function (evt) {
-    evt.preventDefault();
-    $.get('/search', get_params(), function(data) {
-        $('#results').html(data.results.map(format));
-        $('#results-area').css('visibility', 'visible');
+    function get_params() {
+        return {
+            artist: $('#artist').val(),
+            title: $('#title').val(),
+            genre: $('#genre').val(),
+            year_from: $('#year_from').val(),
+            year_to: $('#year_to').val(),
+        };
+    }
+
+    $('#search-form').submit(function (evt) {
+        evt.preventDefault();
+        $.get('/search', get_params(), function(data) {
+            $('#results').html(data.results.map(format));
+            $('#results-area').css('visibility', 'visible');
+        });
     });
+
 });
