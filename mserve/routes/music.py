4537ef6e53bc4f9121c67a724826fcaadaf15230
Nick Spinale <spinalen@carleton.edu>
2017-01-09 20:17:15 -0500

    allow auth to be disabled

--- mserve/routes/music.py
+++ mserve/routes/music.py
@@ -11,14 +11,16 @@ from mserve.zip import do_zip
 
 
 def check_auth():
-    return 'auth' in request.cookies and get_st().auth.check_auth(request.cookies['auth'])
+    return app.config.get('NO_AUTH') or (
+            'auth' in request.cookies
+            and get_st().auth.check_auth(request.cookies['auth'])
+            )
 
 
 def with_auth(f):
     @wraps(f)
     def g(*args, **kwargs):
         if check_auth():
-        # if True:
             return f(*args, **kwargs)
         else:
             return redirect('/auth')
